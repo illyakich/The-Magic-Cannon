093af7fdfdeaebae4d862303c6da2230441efad3
illyakich <speedfeed123@gmail.com>
2024-09-23 22:32:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿for (int x = 1; x != 101; x++)
+    if (x % 3 == 0 && x % 5 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{x} BOTH");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    else if (x % 3 == 0)
+        {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{x} Fire");
+        Console.ForegroundColor = ConsoleColor.White;
+        }
+        else if (x % 5 == 0)
+        {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{x} Electric");
+        Console.ForegroundColor = ConsoleColor.White;
+        }
+   else
+    {
+        Console.WriteLine($"{x} Normal");
+    }
